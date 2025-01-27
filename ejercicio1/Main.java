7bd26cff7535b1ac66b25c4816db15a790a8b627
Martin Roncero <mroncero@indra.es>
2025-01-27 16:47:43 +0100

    Cambios finales ultima versión

--- ejercicio1/Main.java
+++ ejercicio1/Main.java
@@ -5,27 +5,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        
+        boolean res;
+        do {
+            System.out.println("Ingrese palabra");
+            String str = scn.nextLine();
+            res = true;
+            int len = str.length();
 
-        System.out.println("Ingrese palbra");
-        String str = scn.nextLine();
-        boolean res = true;
-        int len = str.length();
-
-        str = str.toLowerCase();
-        for(int i = 0; i < len/2 && res ; i++){
-            if (str.charAt(i) != str.charAt(len-1-i)) {
-                res = false;
+            str = str.toLowerCase();
+            for (int i = 0; i < len / 2 && res; i++) {
+                if (str.charAt(i) != str.charAt(len - 1 - i)) {
+                    res = false;
+                }
             }
-        }
 
-        if(res){
-            System.out.println(str +": Es palíndromo");
-        } else {
-            System.out.println(str +": No es palíndromo");
-        }
+            if (res) {
+                System.out.println(str + ": Es palíndromo");
+            } else {
+                System.out.println(str + ": No es palíndromo");
+            }
+        } while(!res);
 
         scn.close();
-        
+
     }
 }
